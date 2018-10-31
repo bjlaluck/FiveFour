a0c4a948390f17a7a1b1516cfbf6f67789056a45
Bohdan Laluck <bohdanlaluck@hotmail.com>
2018-10-30 22:03:26 -0400

    5.11 fix emial 4

--- app/mailers/newuser_mailer.rb
+++ app/mailers/newuser_mailer.rb
@@ -1,7 +1,7 @@
 class NewuserMailer < ApplicationMailer
   default from: "bohdanlaluck@hotmail.com"
 
-  def contact_form(email, name)
+  def new_user_form(email, name)
       mail(from: "bohdanlaluck@hotmail.com",
          to: email,
          subject: "Thanks for signing up #{name}")
